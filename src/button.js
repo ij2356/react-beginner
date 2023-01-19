49e86a32f90c6f5272bfa2635fae06931fecb6d5
KSM <ij2356@m-sync.co.kr>
2023-01-19 18:48:02 +0900

    useEffect

--- src/button.js
+++ src/button.js
@@ -1,26 +1,43 @@
 import style from "./button.module.css"
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import React from "react";
 
 function Button({ text }) {
 	const [counter, setCounter] = useState(0);
+	const [keyword, setKeyword] = useState("");
 	const counterClick = () => {
-		setCounter(counter + 1);
+		setCounter((pre) => pre + 1);
 	}
-	const counterClick1 = (e) => { 
-		let a = "10"; 
-		console.log(this); //undefined
-		console.log(e.target); //요소반환
-		e.target.innerText =a ;
+	const onchange = (e) => {
+		setKeyword(e.target.value);
 		
 	}
+	
+
+	
+	useEffect(() => { console.log("dsa") }, []);
+	useEffect(() => {
+		if(keyword != "" && keyword.length > 5)
+		console.log("state change " + keyword)
+	}, [keyword]);
+	useEffect(() => {
+		if(counter > 5)
+		console.log("state change " + counter)
+	}, [counter]);
+
+
+
 
 	return (
 		<>
+			<input value={keyword} onChange={onchange} type="text" placeholder="Search here" />
 			<div className="count-box">
+				
+				현잿개수 
 				<span className="left txt32">{counter}</span>
 				<span className="right">{counter + 1}</span>
 			</div>
-			<button className={style.btn} onClick = {counterClick1}>HEllo {text}</button>
+			<button className={style.btn} onClick = {counterClick}>HEllo {text}</button>
 		</>
 	)
 	
